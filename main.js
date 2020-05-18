d83305fe97b46941894a198e7c26b6c42cbe9119
Carmen Dukes <carmendukes@gmail.com>
2020-05-17 17:37:04 -0700

    update css change function updateSignInStatus

--- main.js
+++ main.js
@@ -50,13 +50,13 @@ function updateSignInStatus(isSignedIn) {
         authorizeButton.style.display = 'none';
         signoutButton.style.display = 'block';
         content.style.display = 'block';
-        authorizeButton.style.display = 'block';
+        videoContainer.style.display = 'block';
         getChannel(defaultChannel);
     } else {
         authorizeButton.style.display = 'block';
         signoutButton.style.display = 'none';
         content.style.display = 'none';
-        authorizeButton.style.display = 'none';
+        videoContainer.style.display = 'none';
     }
 }
 

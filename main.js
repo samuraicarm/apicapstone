3a51cef40a5b5a4dcc1eb4ee552470a13a4901da
Carmen Dukes <carmendukes@gmail.com>
2020-05-19 16:44:06 -0700

    update api call

--- main.js
+++ main.js
@@ -102,12 +102,67 @@ function  showChannelData(data) {
     channelData.innerhtml = data;
 }
 
-//get channel from API
+
+
+function formatQueryParams(params) {
+    const queryItems = Object.keys(params)
+      .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    return queryItems.join('&');
+  }
+  
+  
+  function getYouTubeVideos(query, maxResults=10) {
+    const params = {
+      key: apiKey,
+      part: 'snippet,contentDetails,statistics',
+      forUsername: channel
+    };
+    const queryString = formatQueryParams(params)
+    const url = channelUrl + '?' + queryString;
+  
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error(response.statusText);
+      })
+      .then(responseJson => console.log(JSON.stringify(responseJson)))
+      .catch(err => {
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+  }
+
+  function displayResults(responseJson) {
+    console.log(responseJson);
+  
+    // if there are previous results, remove them
+    $('#results-list').empty();
+  
+    // iterate through the items array
+    for (let i = 0; i < responseJson.items.length; i++){
+  
+      // for each video object in the items 
+      // array, add a list item to the results 
+      // list with the video title, description,
+      // and thumbnail
+      $('#results-list').append(
+        `<li><h3>${responseJson.items[i].snippet.title}</h3>
+        <p>${responseJson.items[i].snippet.description}</p>
+        <img src='${responseJson.items[i].snippet.thumbnails.default.url}'>
+        </li>`
+      )};
+  
+    // display the results section  
+    $('#results').removeClass('hidden');
+  };
+
+/* get channel from API
 function getChannel(channel){
     console.log(channel)
     gapi.client.youtube.channels.list({
         part: 'snippet,contentDetails,statistics',
-        id: channel
+        forUsername: channel
        
     })
         .then(response => {
@@ -143,6 +198,7 @@ function getChannel(channel){
    function numberWithCommas (x) {
        return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
    }
+   */
 
    //request videos
    function requestVideoPlaylist(playlistId) {
